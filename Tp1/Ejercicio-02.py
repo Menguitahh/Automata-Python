3b5e79daf9f153740b06646010cf8cd37e8093b3
Joaquín Caparros <jj.caparros@alumno.um.edu.ar>
2025-03-31 18:52:06 -0300

    Ejercicio 2 hecho

---
+++ Tp1/Ejercicio-02.py
@@ -0,0 +1,12 @@
+
+
+
+def solve(expression: str) -> int:
+    return int(eval(expression))
+
+
+#!Ejemplos
+print(solve("2 + 7 * 2 + 1"))  
+print(solve("2 * 2 * 2 + 32 * 2"))  
+print(solve("2 * 2 * 2 + 32 * 10"))
+
