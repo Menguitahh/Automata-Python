4a08ebcc0735c700d893abf490b48ca8b52ad5bc
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-03-31 20:56:12 -0300

    Mejoras en el ejercicio 2

--- Tp1/Ejercicio-02.py
+++ Tp1/Ejercicio-02.py
@@ -1,12 +1,28 @@
-
+import re
 
 
 def solve(expression: str) -> int:
-    return int(eval(expression))
+    numbers = list(map(int, re.findall(r'\d+', expression)))
+    operators = re.findall(r'[+*]', expression)
+    
+    
+    i = 0
+    while i < len(operators):
+        if operators[i] == '*':
+            numbers[i] *= numbers[i + 1]
+            del numbers[i + 1]
+            del operators[i]
+        else:
+            i += 1
+    
+    
+    result = numbers[0]
+    for i in range(len(operators)):
+        result += numbers[i + 1]
+    
+    return result
 
 
-#!Ejemplos
-print(solve("2 + 7 * 2 + 1"))  
+print(solve("2 + 7 * 2 + 1"))   
 print(solve("2 * 2 * 2 + 32 * 2"))  
-print(solve("2 * 2 * 2 + 32 * 10"))
-
+print(solve("2 * 2 + 2"))  
