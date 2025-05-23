4839c3d580d2f3e3f5a3721c012f9a0daad1ca8a
Joaquín Caparros <105978542+JoaquinCaparros711@users.noreply.github.com>
2025-05-23 17:48:23 -0300

    Added Tp4

---
+++ Tp4/music.py
@@ -0,0 +1,41 @@
+import re
+import csv
+
+    
+
+
+
+
+
+def menu():
+    while True:
+        print("\n--- MENÚ PRINCIPAL ---")
+        print("1. Buscar por Título o Artista")
+        print("2. Mostrar todas las canciones ordenadas por reproducciones (descendente)")
+        print("3. Mostrar Top 10 canciones de un Artista")
+        print("4. Insertar nuevo registro (manual o desde archivo)")
+        print("5. Mostrar Álbumes de un Artista")
+        print("6. Salir")
+        print("----------------------")
+
+        option = input("seleccione una opcion: ")
+
+        if option == "1":
+            print("Cerrando la app...")
+        elif option == "2":
+            print("Cerrando la app...")
+        elif option == "3":
+            print("Cerrando la app...")
+        elif option == "4":
+            print("Cerrando la app...")
+        elif option == "5":
+            print("Cerrando la app...")
+        elif option == "6":
+            print("Cerrando la app...")
+            break
+        else:
+            print("Opción inválida.")
+
+
+if __name__ == "__main__":
+    menu()
