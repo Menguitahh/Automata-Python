b876750988b22eeb35e44b1aba0bdfec80058e5d
AxelG <screaxgameplays@gmail.com>
2025-05-23 18:48:21 -0300

    funcion top 10 artista

--- Tp4/music.py
+++ Tp4/music.py
@@ -1,10 +1,42 @@
 import re
 import csv
+from datetime import timedelta
 
+# Funcion para convertir milisegundos a horas, minutos y segundos
+def convert_duration(ms):
+    try:
+        seconds = int(ms) // 1000
+        return str(timedelta(seconds=seconds))
+    except:
+        return "00:00:00"
     
+def top_10_artista():
+    nombre = input("Ingresa el artista: ")
+    canciones = []
 
+    with open("spotify_and_youtube.csv", "r", encoding="utf-8") as archivo:
+        lector = csv.DictReader(archivo)
+        for fila in lector:
+            if re.search(nombre, fila["Artist"], re.IGNORECASE): #re.IGNORECASE es para indicar q la busqueda no distinga entre mayusculas y minusculas
+                canciones.append(fila)
 
+    if not canciones:
+        print("No se encontraron canciones")
+        return
 
+    def vistas_en_float(fila):
+        try:
+            return float(fila["Views"]) #asegura que se puede ordenar correctamente por vistas, incluso si los datos tienen errores
+        except (ValueError, TypeError):
+            return 0
+
+    top_10 = sorted(canciones, key=vistas_en_float, reverse=True)[:10]
+
+    print(f"\n Top 10 canciones más reproducidas de {nombre.title()}:\n")
+    for i, p in enumerate(top_10, 1):
+        duracion = convert_duration(p["Duration_ms"])
+        vistas = float(p["Views"]) / 1_000_000 if p["Views"] else 0
+        print(f"{i}. {p['Artist']} - {p['Track']} | Duración: {duracion} | Reproducciones: {round(vistas, 2)} millones")
 
 
 def menu():
